5b5a4d4f133a96cd6e32f6b34edfc8d6a2a83a36
Angelo Pinheiro <angelorpsantos@gmail.com>
2017-12-31 19:11:32 +0000

    criando o constantes_pts.php

---
+++ constantes_pts.php
@@ -0,0 +1,17 @@
+<?php	
+	/**
+	*	Aqui estao setadas todas constantes que representam 
+	*	os valores em pontos para cada requisito, secao...
+	*
+	*
+	**/
+	
+	//Constante de pontuacao base (em caso de nao match com os requisitos)
+	define("PONTOS_INDIFERENTE", 2.0);
+	//Constantes avaliativas
+	define("PONTOS_PESSOAL", 4.0);
+	define("PONTOS_ESTADO_CIVIL", 5.0);
+	define("PONTOS_LOCALIZACAO", 8.0);
+	define("PONTOS_SAL_PRETEN", 8.0);
+	define("PONTOS_FORMACAO", 9.0);
+	define("PONTOS_CONHECIMENTOS", 10.0);		

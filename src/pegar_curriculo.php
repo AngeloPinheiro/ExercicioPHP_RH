65b86d0c656cf47e2bce824021474dfe16d560d5
Angelo Pinheiro <angelorpsantos@gmail.com>
2017-12-31 20:26:13 +0000

    preparacao dos dados do form concluido

--- src/pegar_curriculo.php
+++ src/pegar_curriculo.php
@@ -4,6 +4,9 @@
 	*	retorna os dados em um array associativo
 	*/
 	
+	require_once 'pegar_post.php';
+	require_once 'decodificar.php';
+
 	function pegar_curriculo(){
 		$json = pegar_post();
 		decodificar($json);
